9b5bafc8fb7de8a4ff82b726d05edd7d1d74443e
Anna Genina <agenina@freya01.bc.rzg.mpg.de>
2025-04-18 00:55:08 +0200

    typo

--- AnalysisCode/gs_analysis.py
+++ AnalysisCode/gs_analysis.py
@@ -73,7 +73,7 @@ np.random.shuffle(chains)
 num_ch = np.random.choice(np.arange(len(chains)),samples, replace = False)
 chains = chains[num_ch]
 ch_org = len(chains)
-print('Selected %d samples' %ch_org
+print('Selected %d samples' %ch_org)
 
 print('Running beta')
 analysis_func.return_beta(chains, options, priors, min_r, max_r, points,codedir,workdir,project_name, galaxy)
